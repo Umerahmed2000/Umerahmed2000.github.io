c46c35324059f6a2678f4410f6d22b892b104377
Umerahmed2000 <91253327+Umerahmed2000@users.noreply.github.com>
2021-10-13 23:32:21 +0500

    Delete mod4 assignment directory

--- mod4 assignment/speakgoodbye.js
+++
@@ -1,8 +0,0 @@
-(function(window){
-    var speakbye={};
-    var speakword= "Good Bye";
-    speakbye.saybye=function speak(name) {
-      console.log(speakbye+" "+name);
-    }
-    window.speakbye= speakbye ; 
-})(window);
--- mod4 assignment/speakhello.js
+++
@@ -1,8 +0,0 @@
-(function(window){
-var hellospeak={};
-var speakWord = "Hello";
-hellospeak.hellosay= function speak(name) {
-  console.log(speakWord+" " +name  );
-}
-window.hellospeak= hellospeak;
-})(window);
